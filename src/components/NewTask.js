147d0e2917612b22b301893417cd73ccd600884e
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-05-19 17:44:22 -0400

    Add stopwatch component

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,12 +1,22 @@
 import classes from "./NewTask.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import Stopwatch from "./Stopwatch";
 
 const NewTask = () => {
-    return (
-        <div className={classes.container}>
-            <input className={classes.input} placeholder='What are you about to do?'></input>
-            <button className={classes.btn}>></button>
-        </div>
-    )
-}
+  return (
+    <div className={classes.container}>
+      <input
+        className={classes.input}
+        type='text'
+        placeholder="What are you about to do?"
+      ></input>
+      <Stopwatch />
+      <button className={classes.btn}>
+        <FontAwesomeIcon icon={faPlay} />
+      </button>
+    </div>
+  );
+};
 
 export default NewTask;
---
+++ src/components/Stopwatch.js
@@ -0,0 +1,11 @@
+import classes from "./Stopwatch.module.css";
+
+const Stopwatch = () => {
+  return (
+    <>
+      <p className={classes.time}>00:00:00</p>
+    </>
+  );
+};
+
+export default Stopwatch;
