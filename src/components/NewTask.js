832af3216e94107e415cd291574fe31d1dd9f6ca
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-05-20 17:50:22 -0400

    Add toggle button functionality

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -2,34 +2,73 @@ import classes from "./NewTask.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
 import Stopwatch from "./Stopwatch";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const NewTask = () => {
-  const [stopwatchIsRunning, setStopwatchIsRunning] = useState(false);
   console.log("NewTask.js re-rendered.");
-  const toggleStopwatch = () => {
-    setStopwatchIsRunning(!stopwatchIsRunning);
+  const [stopwatchIsRunning, setStopwatchIsRunning] = useState(false);
+  const [task, setTask] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [statusMessage, setStatusMessage] = useState("");
+  const [showDoneButton, setShowDoneButton] = useState(false);
+
+  const changeHandler = (e) => {
+    setTask(e.target.value);
   };
 
+  const toggleStopwatch = () => {
+    if (task.trim().length === 0) {
+      setStatusMessage("Task field cannot be empty!");
+      return;
+    }
+    setStatusMessage("");
+    setStopwatchIsRunning(!stopwatchIsRunning);
+    setIsDisabled(true);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (stopwatchIsRunning) {
+        setShowDoneButton(true);
+      } else {
+        setShowDoneButton(false);
+      }
+    };
+  }, [stopwatchIsRunning]);
+
   return (
-    <div className={classes.container}>
-      <input
-        className={classes.input}
-        type="text"
-        placeholder="What are you about to do?"
-      ></input>
-      <Stopwatch stopwatchIsRunning={stopwatchIsRunning} />
-      <button
-        className={classes.btn}
-        onClick={toggleStopwatch}
-        style={{ backgroundColor: stopwatchIsRunning ? "#dd7777" : "#77dd77" }}
+    <div>
+      <div
+        className={classes.container}
+        style={{
+          boxShadow: stopwatchIsRunning ? "0px 0px 15px #01315A" : "",
+        }}
       >
-        {stopwatchIsRunning ? (
-          <FontAwesomeIcon icon={faPause} />
-        ) : (
-          <FontAwesomeIcon icon={faPlay} />
-        )}
-      </button>
+        <input
+          className={classes.input}
+          type="text"
+          placeholder="What are you about to do?"
+          value={task}
+          onChange={changeHandler}
+          disabled={isDisabled}
+        ></input>
+        <Stopwatch stopwatchIsRunning={stopwatchIsRunning} />
+        <button
+          className={classes.btn}
+          onClick={toggleStopwatch}
+          style={{
+            backgroundColor: stopwatchIsRunning ? "#dd7777" : "#77dd77",
+          }}
+        >
+          {stopwatchIsRunning ? (
+            <FontAwesomeIcon icon={faPause} />
+          ) : (
+            <FontAwesomeIcon icon={faPlay} />
+          )}
+        </button>
+      </div>
+      <p className={classes.status}>{statusMessage}</p>
+      {showDoneButton && <DoneButton />}
     </div>
   );
 };
