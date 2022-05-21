67974d6ca2583ba32f201adedf2864a5bee825ea
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-05-21 12:29:53 -0400

    Add log styling

--- src/components/LoggedTasks.js
+++ src/components/LoggedTasks.js
@@ -3,17 +3,21 @@ import classes from "./LoggedTasks.module.css";
 const LoggedTasks = () => {
   return (
     <div className={classes.container}>
-      <div className={classes["left-container"]}>
-        <h1 className={classes.date}>
-          Today,<br></br> <span>May 21</span>
-        </h1>
-      </div>
+      <div className={classes["header-container"]}>
+        <div className={classes["left-container"]}>
+          <h1 className={classes.date}>
+            Today,<br></br> <span>May 21</span>
+          </h1>
+        </div>
 
-      <div className={classes["right-container"]}>
-        <h2 className={classes.total}>Total: 06:38:52</h2>
-        <button className={classes.filter} type="button">
-          Filter
-        </button>
+        <div className={classes["right-container"]}>
+          <h2 className={classes.total}>
+            Total: <span>06:38:52</span>
+          </h2>
+          <button className={classes["filter-btn"]} type="button">
+            Filter
+          </button>
+        </div>
       </div>
       <hr className={classes.line}></hr>
     </div>
