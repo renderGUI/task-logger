6d424be0df8543dc191513dff99a1b2010864178
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-06-16 08:15:33 -0400

    Add task log pagination

--- src/components/LoggedTasks.js
+++ src/components/LoggedTasks.js
@@ -1,23 +1,47 @@
 import classes from "./LoggedTasks.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { tasksContext } from "../contexts/tasksContext";
 import DeleteTask from "./DeleteTask";
+import ReactPaginate from "react-paginate";
 
 const LoggedTasks = (props) => {
   const { tasks } = useContext(tasksContext);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const tasksPerPage = 5;
+  const pagesVisited = pageNumber * tasksPerPage;
+
+  const displayTasks = tasks
+    .slice(pagesVisited, pagesVisited + tasksPerPage)
+    .map((task) => {
+      return (
+        <div className={classes.container} key={task.id}>
+          <p className={classes.task}>{task.loggedTask}</p>
+          <p className={classes.time}>{task.loggedTime}</p>
+          {props.editIsEnabled && <DeleteTask id={task.id} />}
+        </div>
+      );
+    });
+
+  const pageCount = Math.ceil(tasks.length / tasksPerPage);
+
+  const changePageHandler = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   return (
     <>
-      {tasks.length > 0 &&
-        tasks.map((task) => {
-          return (
-            <div className={classes.container} key={task.id}>
-              <p className={classes.task}>{task.loggedTask}</p>
-              <p className={classes.time}>{task.loggedTime}</p>
-              {props.editIsEnabled && <DeleteTask id={task.id} />}
-            </div>
-          );
-        })}
+      {tasks.length > 0 && displayTasks}
+      {tasks.length > 0 && (
+        <ReactPaginate
+          previousLabel={"<"}
+          nextLabel={">"}
+          pageCount={pageCount}
+          onPageChange={changePageHandler}
+          containerClassName={classes.paginationButtons}
+          activeClassName={classes.paginationActive}
+        />
+      )}
       {tasks.length === 0 && (
         <div className={classes["empty-list-container"]}>
           <p>No logged tasks today!</p>
